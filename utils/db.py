2068f274e79c0616f7ba101529b6c3039c7e44fb
Kidblip <140572342+Kidblip@users.noreply.github.com>
2025-11-08 21:43:51 +0100

    second commit

--- utils/db.py
+++ utils/db.py
@@ -2,8 +2,8 @@ import os
 from sqlalchemy import create_engine, MetaData
 from databases import Database
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "../../gpas.db")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # path to utils
+DB_PATH = os.path.join(BASE_DIR, "..", "gpas.db")      # one folder up (backend/gpas.db)
 DATABASE_URL = f"sqlite:///{os.path.abspath(DB_PATH)}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
